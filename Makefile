663751cc5e955fc3bae0a39a25c8ce67f9f22e71
Arjaz <arjaz@protonmail.com>
2019-01-12 13:11:39 +0200

    Reading, deleting and getting help from terminal added

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ Task.cpp List.cpp main.cpp -lboost_system -lboost_serialization -Wall
--- main.cpp
+++ main.cpp
@@ -3,17 +3,58 @@
 #include "Task.h"
 #include "List.h"
 
-std::string about() {
+constexpr const char* about() {
     return "This is a console todo list used to store different tasks.";
 }
 
+constexpr const char* errmsg() {
+    return "Type 'h' for help, 'r' for reading, 'w' <TASK_NAME> <TASK_CONTENT> for writing, 'd' for deleting the list.";
+}
+
+constexpr const char* errload() {
+    return "List is not created.";
+}
+
 int main(int argc, char *argv[]) {
-
-    std::cout << argc << std::endl;
-
     if (argc == 1) {
         std::cout << about() << std::endl;
         return 0;
+    }
+    
+    List lst;
+    lst.setName("Fuck");
+    lst.addTask("Kiss", "Kiss Helga");
+    lst.addTask("App", "Create Qt youtube desktop application");
+    lst.save("lst.obj");
+
+    switch(*argv[1]) {
+        case 'r':
+            try {
+                lst.load("lst.obj");
+            }
+            catch (boost::archive::archive_exception &e) {
+                std::cout << errload() << std::endl << errmsg() << std::endl;
+                return -1;
+            }
+            for (auto x: lst.getTasks()) {
+                std::cout << x.getName() << ":\t" << x.getContent() << std::endl;
+            }
+            break;
+        case 'w':
+            if (argc == 2) {
+                std::cout << errmsg() << std::endl;
+            }
+            break;
+        case 'd':
+            remove("lst.obj");
+            std::cout << "List was deleted" << std::endl;
+            break;
+        case 'h':
+            std::cout << about() << std::endl << errmsg() << std::endl;
+            break;
+        default:
+            std::cout << errmsg() << std::endl;
+            break;
     }
 
     return 0;
